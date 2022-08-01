de47862dae68e197a3425491d138a1da72d9376c
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-08-01 07:53:57 -0300

    Le agarre la mano

--- 22. Catalogo de Peliculas/servicio/CatalogoPeliculas.py
+++ 22. Catalogo de Peliculas/servicio/CatalogoPeliculas.py
@@ -1,5 +1,24 @@
-from dominio.Pelicula import Pelicula
 import os
 
 class CatalogoPeliculas:
     rutaArchivo = 'peliculas.txt'
+    
+    def agregarPeliculas(Pelicula):
+        rutaArchivo = open('peliculas.txt', 'a')
+        rutaArchivo.write(f'{Pelicula}\n')
+        rutaArchivo.close()
+    
+    def listarPeliculas():
+        with open('peliculas.txt') as f:
+            print(f.read())
+    
+    def eliminar():
+        os.remove('peliculas.txt')
+    
+    
+h = CatalogoPeliculas
+h.eliminar()
+
+
+
+
