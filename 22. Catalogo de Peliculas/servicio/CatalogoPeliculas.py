dc4c3e8e3426d1be1d7ff9e78da8a29c22c9efca
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-08-01 09:29:02 -0300

    Completada la actividad con exito

--- 22. Catalogo de Peliculas/servicio/CatalogoPeliculas.py
+++ 22. Catalogo de Peliculas/servicio/CatalogoPeliculas.py
@@ -1,5 +1,4 @@
 import os
-from dominio import Pelicula
 
 class CatalogoPeliculas:
     rutaArchivo = 'peliculas.txt'
--- 22. Catalogo de Peliculas/test.py
+++ 22. Catalogo de Peliculas/test.py
@@ -1,27 +1,32 @@
 from servicio.CatalogoPeliculas import CatalogoPeliculas
+from dominio.Pelicula import Pelicula
 
 catalogo = CatalogoPeliculas
 
 def menu():
     while(True):
-        print("1. Aniadir pelicula")
-        print("2. Listar peliculas")
-        print("3. Eliminar listado")
-        print("4. Salir")
-        print()
-        
-        c = int(input("Seleccione a continuacion que opcione le sienta mejor: "))
-        
-        if c == 1:
-            a = input("Cual pelicula desea aniadir? ")
-            catalogo.agregarPelicula(a)
-        elif c == 2:
-            catalogo.listarPeliculas()
-        elif c == 3:
-            catalogo.eliminar()
-        elif c == 4:
-            print("Adios gente")
-            exit(0)
+        try:
+            print("1. Aniadir pelicula")
+            print("2. Listar peliculas")
+            print("3. Eliminar listado")
+            print("4. Salir")
+            print()
+            
+            c = int(input("Seleccione a continuacion que opcione le sienta mejor: "))
+            
+            if c == 1:
+                a = input("Cual pelicula desea aniadir? ")
+                pelicula = Pelicula(a)
+                catalogo.agregarPelicula(pelicula)
+            elif c == 2:
+                catalogo.listarPeliculas()
+            elif c == 3:
+                catalogo.eliminar()
+            elif c == 4:
+                print("Adios gente")
+                exit(0)
+        except Exception:
+            print("Eliga un caracter valido porque de otro modo programa boom")
 
 menu()
         
