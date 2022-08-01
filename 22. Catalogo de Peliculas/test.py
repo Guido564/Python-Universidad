9473cea91e68f5482790a47c9edd61fc55ff501e
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-08-01 09:09:12 -0300

    Esta quedando lindo

--- 22. Catalogo de Peliculas/test.py
+++ 22. Catalogo de Peliculas/test.py
@@ -1,20 +1,27 @@
 from servicio.CatalogoPeliculas import CatalogoPeliculas
 
-c = CatalogoPeliculas
+catalogo = CatalogoPeliculas
 
-n = int(input("Diga que quiere hacer: "))
+def menu():
+    while(True):
+        print("1. Aniadir pelicula")
+        print("2. Listar peliculas")
+        print("3. Eliminar listado")
+        print("4. Salir")
+        print()
+        
+        c = int(input("Seleccione a continuacion que opcione le sienta mejor: "))
+        
+        if c == 1:
+            a = input("Cual pelicula desea aniadir? ")
+            catalogo.agregarPelicula(a)
+        elif c == 2:
+            catalogo.listarPeliculas()
+        elif c == 3:
+            catalogo.eliminar()
+        elif c == 4:
+            print("Adios gente")
+            exit(0)
 
-while n >= 1 and n <= 4:
-    if n == 1:
-        aniadir = input("A continuacion nombre la pelicula a agregar: ")
-        c.agregarPeliculas(aniadir)
-        break
-    if n == 2:
-        c.listarPeliculas()
-        break
-    if n == 3:
-        c.eliminar()
-        break
-    continue
-    
+menu()
         
