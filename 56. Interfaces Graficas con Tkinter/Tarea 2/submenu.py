32b3f5267e55156d8f4d9eed4182da9fc3ebb555
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-08-05 12:25:20 -0300

    Podes salir :D

--- 56. Interfaces Graficas con Tkinter/Tarea 2/submenu.py
+++ 56. Interfaces Graficas con Tkinter/Tarea 2/submenu.py
@@ -1,3 +1,4 @@
+import sys
 import tkinter as tk
 from tkinter import ttk, messagebox, Menu
 
@@ -5,6 +6,10 @@ ventana = tk.Tk()
 ventana.geometry('600x600')
 ventana.title('Manejo de Componentes')
 
+def salir():
+    ventana.quit()
+    ventana.destroy()
+    sys.exit()
 
 def crearMenu():
     menuPrincipal = Menu(ventana)
@@ -13,7 +18,7 @@ def crearMenu():
     subMenuArchivo.add_command(label='Abrir')
     subMenuArchivo.add_command(label='Guardar')
     subMenuArchivo.add_separator()
-    subMenuArchivo.add_command(label='Salir')
+    subMenuArchivo.add_command(label='Salir', command=salir)
     menuPrincipal.add_cascade(menu=subMenuArchivo, label='Archivo')
     
     subMenuAyuda = Menu(menuPrincipal, tearoff=0)
