7b3828b9ca12c250062f9121f17303eacbef3a28
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-08-05 10:57:51 -0300

    Terminado

--- 56. Interfaces Graficas con Tkinter/Tarea 1/gridmanager.py
+++ 56. Interfaces Graficas con Tkinter/Tarea 1/gridmanager.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import mainloop, ttk
+from tkinter import Button, mainloop, ttk
 
 ventana = tk.Tk()
 ventana.geometry('600x600')
@@ -7,11 +7,22 @@ ventana.title('Progamita humilde')
 
 ventana.rowconfigure(0, weight=1)
 ventana.rowconfigure(1, weight=5)
+ventana.columnconfigure(0, weight=1)
+ventana.columnconfigure(1, weight=5)
 
-boton1 = ttk.Button(ventana, text='Hola soy B1')
+def intenciones():
+    boton1.config(text='Intenciones desconocidas')
+
+boton1 = ttk.Button(ventana, text='Hola soy B1', command=intenciones)
 boton1.grid(row=0, column=0, sticky='NSWE')
 
 boton2 = ttk.Button(ventana, text='Hola soy B2')
 boton2.grid(row=1, column=0, sticky='NSWE')
+
+boton3 = ttk.Button(ventana, text='Hola soy B3')
+boton3.grid(row=0, column=1, sticky='NSWE')
+
+boton4 = ttk.Button(ventana, text='Hola soy B4')
+boton4.grid(row=1, column=1, sticky='NSWE')
 
 mainloop()
