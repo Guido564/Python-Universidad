5dfb9d8341cbce4d59d243f4eb30c6325a76ac59
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-08-05 12:21:55 -0300

    Jugando con menu en Tkinter

---
+++ 56. Interfaces Graficas con Tkinter/Tarea 2/submenu.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from tkinter import ttk, messagebox, Menu
+
+ventana = tk.Tk()
+ventana.geometry('600x600')
+ventana.title('Manejo de Componentes')
+
+
+def crearMenu():
+    menuPrincipal = Menu(ventana)
+    subMenuArchivo = Menu(menuPrincipal, tearoff=0)
+    subMenuArchivo.add_command(label='Nuevo')
+    subMenuArchivo.add_command(label='Abrir')
+    subMenuArchivo.add_command(label='Guardar')
+    subMenuArchivo.add_separator()
+    subMenuArchivo.add_command(label='Salir')
+    menuPrincipal.add_cascade(menu=subMenuArchivo, label='Archivo')
+    
+    subMenuAyuda = Menu(menuPrincipal, tearoff=0)
+    subMenuAyuda.add_command(label='Documentacion')
+    subMenuAyuda.add_command(label='Ayuda')
+    subMenuAyuda.add_command(label='Acerca de')
+    menuPrincipal.add_cascade(menu=subMenuAyuda, label='Informacion')
+    
+    ventana.config(menu=menuPrincipal)
+    
+
+crearMenu()
+ventana.mainloop()
