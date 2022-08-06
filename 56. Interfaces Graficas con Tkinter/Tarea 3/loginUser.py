a7cb4f1414f6d0742d4985258c62bacea5e5407b
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-08-06 15:18:48 -0300

    Convertido en clase el ejercicio de Login

--- 56. Interfaces Graficas con Tkinter/Tarea 3/loginUser.py
+++ 56. Interfaces Graficas con Tkinter/Tarea 3/loginUser.py
@@ -1,42 +1,46 @@
+from distutils.log import Log
 import sys
 import tkinter as tk
 from tkinter import ttk, messagebox
 
-ventana = tk.Tk()
-ventana.geometry('250x100')
-ventana.title('Login')
-ventana.resizable(0, 0)
-
-ventana.rowconfigure(1, weight=1)
-ventana.columnconfigure(1, weight=1)
-
-ventana.rowconfigure(2, weight=1)
-ventana.columnconfigure(2, weight=1)
-
-ventana.rowconfigure(3, weight=1)
-ventana.columnconfigure(3, weight=1)
-
-
-usuarioEt = tk.Label(ventana, text='Usuario:')
-usuarioEt.grid(row=1, column=0, sticky='E')
-usuario = ttk.Entry(ventana, width=30)
-usuario.grid(row=1, column=1, sticky='E')
-
-passEt = tk.Label(ventana, text='Password:')
-passEt.grid(row=2, column=0, sticky='E')
-password = ttk.Entry(ventana, width=30, show='*')
-password.grid(row=2, column=1, sticky='E')
-
-def enviar():
-    usuarioData = usuario.get()
-    pasData = password.get()
+class Login(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry('250x100')
+        self.title('Login')
+        self.resizable(0, 0)
+        self.rowconfigure(1, weight=1)
+        self.columnconfigure(1, weight=1)
+        
+        self.rowconfigure(2, weight=1)
+        self.columnconfigure(2, weight=1)
+        
+        self.rowconfigure(3, weight=1)
+        self.columnconfigure(3, weight=1)
+        
+        self._crearComponentes()
     
-    messagebox.showinfo('Datos del usuario', f'Usuario: {usuarioData}\nContrasenia: {pasData}')
-    
-    enviar1.config(text='Enviado!')
+    def _crearComponentes(self):
+        usuarioEt = tk.Label(self, text='Usuario:')
+        usuarioEt.grid(row=1, column=0, sticky='E')
+        self.usuario = ttk.Entry(self, width=30)
+        self.usuario.grid(row=1, column=1, sticky='E')
 
-enviar1 = ttk.Button(ventana, text='Enviar', command=enviar)
-enviar1.grid(row=3, column=1)
+        passEt = tk.Label(self, text='Password:')
+        passEt.grid(row=2, column=0, sticky='E')
+        self.password = ttk.Entry(self, width=30, show='*')
+        self.password.grid(row=2, column=1, sticky='E')
+        
+        self.enviar1 = ttk.Button(self, text='Enviar', command=self._enviar)
+        self.enviar1.grid(row=3, column=1)
 
+    def _enviar(self):
+        usuarioData = self.usuario.get()
+        pasData = self.password.get()
+        
+        messagebox.showinfo('Datos del usuario', f'Usuario: {usuarioData}\nContrasenia: {pasData}')
+        
+        self.enviar1.config(text='Enviado!')
 
-ventana.mainloop()
+loginVentana = Login()
+loginVentana.mainloop()
