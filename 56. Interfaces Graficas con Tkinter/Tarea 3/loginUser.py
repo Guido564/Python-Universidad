e743f792b22b4f8a7539015ab8e1139c35a333bb
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-08-06 15:05:54 -0300

    Correcciones esteticas

--- 56. Interfaces Graficas con Tkinter/Tarea 3/loginUser.py
+++ 56. Interfaces Graficas con Tkinter/Tarea 3/loginUser.py
@@ -5,9 +5,7 @@ from tkinter import ttk, messagebox
 ventana = tk.Tk()
 ventana.geometry('250x100')
 ventana.title('Login')
-
-ventana.rowconfigure(0, weight=1)
-ventana.columnconfigure(0, weight=1)
+ventana.resizable(0, 0)
 
 ventana.rowconfigure(1, weight=1)
 ventana.columnconfigure(1, weight=1)
@@ -15,16 +13,19 @@ ventana.columnconfigure(1, weight=1)
 ventana.rowconfigure(2, weight=1)
 ventana.columnconfigure(2, weight=1)
 
+ventana.rowconfigure(3, weight=1)
+ventana.columnconfigure(3, weight=1)
+
 
 usuarioEt = tk.Label(ventana, text='Usuario:')
-usuarioEt.grid(row=0, column=0, sticky='E')
+usuarioEt.grid(row=1, column=0, sticky='E')
 usuario = ttk.Entry(ventana, width=30)
-usuario.grid(row=0, column=1, sticky='E')
+usuario.grid(row=1, column=1, sticky='E')
 
 passEt = tk.Label(ventana, text='Password:')
-passEt.grid(row=1, column=0, sticky='E')
+passEt.grid(row=2, column=0, sticky='E')
 password = ttk.Entry(ventana, width=30, show='*')
-password.grid(row=1, column=1, sticky='E')
+password.grid(row=2, column=1, sticky='E')
 
 def enviar():
     usuarioData = usuario.get()
@@ -35,7 +36,7 @@ def enviar():
     enviar1.config(text='Enviado!')
 
 enviar1 = ttk.Button(ventana, text='Enviar', command=enviar)
-enviar1.grid(row=2, column=1)
+enviar1.grid(row=3, column=1)
 
 
 ventana.mainloop()
