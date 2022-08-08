3e334a5877bf90f2f2cf4b99358723f54c1c5736
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-08-08 09:35:22 -0300

    Manejo de data list (googleo incorrecto)

--- 57. Componentes en Tkinter/Tarea 1/tarea.py
+++ 57. Componentes en Tkinter/Tarea 1/tarea.py
@@ -42,22 +42,42 @@ def componentes_tabulador2(tabulador):
     scroll.insert(tk.INSERT, contenido)
     scroll.grid(row=0, column=0)
     
+def componentes_tabulador3(tabulador):
+    etiqueta1 = ttk.Label(tabulador, text='Del 1 al 10 que tanto le gusto el cuento anterior?')
+    etiqueta1.grid(row=0, column=0, padx=5, pady=5)
+    
+    datos = [x + 1 for x in range(11)]
+    combobox = ttk.Combobox(tabulador, width=15, values=datos)
+    combobox.grid(row=1, column=0)
+    
+    def enviar():
+        if len(combobox.get()) == 0:
+            messagebox.showerror('Nanana!', 'Elegi una opcion')
+        else:    
+            messagebox.showinfo('Datos?', f'Opcion seleccionada: {combobox.get()}')
+    
+    boton1 = ttk.Button(tabulador, text='Lo quiere enviar?', command=enviar)
+    boton1.grid(row=2, column=0, padx=5, pady=5)
+    
 def crear_tabs():
     #Tab control
     control_tabulador = ttk.Notebook(ventana)
     #Frame para organizar elementos dentro del tab
     tabulador1 = ttk.Frame(control_tabulador)
     tabulador2 = ttk.LabelFrame(control_tabulador, text='Una pequenia fabula')
+    tabulador3 = ttk.Frame(control_tabulador)
     #Control de tabuladores
     control_tabulador.add(tabulador1, text='El primero de muchos')
     control_tabulador.add(tabulador2, text='El segundo hermano')
+    control_tabulador.add(tabulador3, text='Estoy para datos (3)')
     #Se ve el tabulador lindo
     control_tabulador.pack(fill='both')
     #Creacion componentes tabulador1
     componentes_tabulador1(tabulador1)
     #Creacion componentes tabulador2
     componentes_tabulador2(tabulador2)
-    
+    #Creacion componentes tabulador3
+    componentes_tabulador3(tabulador3)
 
 crear_tabs()
 
