c0ef63acdcb5749a07163abc42a26147e9561008
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-08-08 09:10:33 -0300

    Scroll text trabajo

--- 57. Componentes en Tkinter/Tarea 1/tarea.py
+++ 57. Componentes en Tkinter/Tarea 1/tarea.py
@@ -1,5 +1,7 @@
+from textwrap import wrap
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, scrolledtext
+from turtle import width
 
 ventana = tk.Tk()
 ventana.geometry('600x400')
@@ -32,19 +34,29 @@ def componentes_tabulador1(tabulador):
     boton1 = ttk.Button(tabulador, text='Lo quiere enviar?', command=enviar)
     boton1.grid(row=3, column=0, padx=5, pady=5, columnspan=2)
     
+def componentes_tabulador2(tabulador):
+    contenido = '''«Ay», dijo el ratón, «el mundo se está haciendo más chiquito cada día. Al principio era tan grande que yo tenía miedo, corría y corría, y me alegraba cuando al fin veía paredes a lo lejos a diestra y siniestra, pero estas largas paredes se han achicado tanto que ya estoy en la última cámara, y ahí en la esquina está la trampa a la cual yo debo caer». «Sólamente tienes que cambiar tu dirección», dijo el gato, y se lo comió. 
+                                            -Fran Kafka'''
 
+    scroll = scrolledtext.ScrolledText(tabulador, width=65, height=462, wrap=tk.WORD)
+    scroll.insert(tk.INSERT, contenido)
+    scroll.grid(row=0, column=0)
+    
 def crear_tabs():
     #Tab control
     control_tabulador = ttk.Notebook(ventana)
     #Frame para organizar elementos dentro del tab
-    tabulador1= ttk.Frame(control_tabulador)
-    #Tabulador al control de tabuladores
+    tabulador1 = ttk.Frame(control_tabulador)
+    tabulador2 = ttk.LabelFrame(control_tabulador, text='Una pequenia fabula')
+    #Control de tabuladores
     control_tabulador.add(tabulador1, text='El primero de muchos')
+    control_tabulador.add(tabulador2, text='El segundo hermano')
     #Se ve el tabulador lindo
     control_tabulador.pack(fill='both')
     #Creacion componentes tabulador1
     componentes_tabulador1(tabulador1)
-    
+    #Creacion componentes tabulador2
+    componentes_tabulador2(tabulador2)
     
 
 crear_tabs()
