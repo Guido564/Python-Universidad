3ebe44d0c1383309731a16225b556895f37c23bc
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-08-11 10:38:09 -0300

    Mas o menos se entiende la idea

--- 59. Aplicacion Editor de Texto/Mi version/editor_texto.py
+++ 59. Aplicacion Editor de Texto/Mi version/editor_texto.py
@@ -6,15 +6,20 @@ ventana = tk.Tk()
 ventana.geometry('900x600')
 ventana.title('Hola soy un editor de texto')
 
-def editor_texto():
-    texto = Text(ventana, width=112, height=600, wrap=tk.WORD)
-    texto.grid(row=0, column=0)
+texto = Text(ventana, width=112, height=600, wrap=tk.WORD)
+texto.grid(row=0, column=0)
+
+def editar():
+    esto_se_guarda = texto.get('1.0','end-1c')
+    with open('guardado_default.txt', 'w') as g:
+        g.write(esto_se_guarda)
+
     
 def crear_menu():
     menuPrincipal = Menu(ventana)
     subMenuArchivo = Menu(menuPrincipal, tearoff=0)
     subMenuArchivo.add_command(label='Abrir')
-    subMenuArchivo.add_command(label='Guardar')
+    subMenuArchivo.add_command(label='Guardar', command=editar)
     subMenuArchivo.add_command(label='Guardar como...')
     subMenuArchivo.add_separator()
     subMenuArchivo.add_command(label='Salir')
@@ -23,5 +28,4 @@ def crear_menu():
     ventana.config(menu=menuPrincipal)
 
 crear_menu()
-editor_texto()
 ventana.mainloop()
