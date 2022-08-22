3a7fed1ea81f320e1f53be8b1698fc987fae1117
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-08-21 22:23:21 -0300

    Movimiento de archivos y finalizacion

--- 64. Barras de Herramientas y Menus en PySide/Barra de Herramientas/barra.py
+++ 64. Barras de Herramientas y Menus en PySide/Barra de Herramientas/barra.py
@@ -1,5 +1,5 @@
-from PySide6.QtCore import Qt
-from PySide6.QtGui import QAction
+from PySide6.QtCore import Qt, QSize
+from PySide6.QtGui import QAction, QIcon
 from PySide6.QtWidgets import QMainWindow, QApplication, QLabel, QToolBar, QStatusBar
 
 
@@ -15,21 +15,29 @@ class Ventana(QMainWindow):
         self.setCentralWidget(etiqueta)
         
         barra = QToolBar('Soy una barra de herramientas')
+        barra.setIconSize(QSize(16,16))
         self.addToolBar(barra)
         
-        boton = QAction('Boton', self)
-        barra.addAction(boton)
+        boton_nuevo = QAction(QIcon('Barra de Herramientas\\nuevo.png'), 'Nuevo', self)
+        barra.addAction(boton_nuevo)
+        
+        boton_guardado = QAction(QIcon('Barra de Herramientas\\guardar.png'), 'Guardar', self)
+        barra.addAction(boton_guardado)
         
         self.setStatusBar(QStatusBar(self))
         
-        boton.setStatusTip('Buen dia, que se le ofrece?')
+        boton_nuevo.setStatusTip('Hola ha presionado el boton de nuevo, que se le ofrece?')
+        boton_guardado.setStatusTip('Hola ha presionado el boton de guardado, que se le ofrece?')
         
-        boton.triggered.connect(self.click_boton)
+        boton_nuevo.triggered.connect(self.click_boton_nuevo)
+        boton_guardado.triggered.connect(self.click_guardado)
         
-        boton.setCheckable(True)
         
-    def click_boton(self, s):
-        print(f'Recibiendo click: {s}')
+    def click_boton_nuevo(self, s):
+        print(f'Nuevo archivo: {s}')
+        
+    def click_guardado(self, s):
+        print(f'Guardar archivo: {s}')
 
 if __name__ == '__main__':    
     app = QApplication([])
