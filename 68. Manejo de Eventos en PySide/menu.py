cbb1e21fa08a2e4fcffa54196fabfdf344b49c58
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-08-22 07:36:14 -0300

    El programa reconoce el click presionado

--- 68. Manejo de Eventos en PySide/menu.py
+++ 68. Manejo de Eventos en PySide/menu.py
@@ -1,3 +1,4 @@
+from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
 
 class VentanaPrincipal(QMainWindow):
@@ -7,17 +8,36 @@ class VentanaPrincipal(QMainWindow):
         self.etiqueta = QLabel('Si es tan amable salude a esta ventana con un pituco click')
         self.setCentralWidget(self.etiqueta)
         
-    def mouseMoveEvent(self, event):
-        self.etiqueta.setText('Te vas a marear, tio')
+    def mouseMoveEvent(self):
+        self.etiqueta.setText('Te vas a marear, tio (Click Izq)')
         
+    
     def mousePressEvent(self, event):
-        self.etiqueta.setText('Click!')
+        if event.button() == Qt.LeftButton:
+            self.etiqueta.setText('Click! (Click Izq)')
+        elif event.button() == Qt.MiddleButton:
+            self.etiqueta.setText('Click! (Click Middle)')
+        elif event.button() == Qt.RightButton:
+            self.etiqueta.setText('Click! (Click Right)')
+
         
     def mouseReleaseEvent(self, event):
-        self.etiqueta.setText('Haga click click!')
+        if event.button() == Qt.LeftButton:
+            self.etiqueta.setText('Haga click click! (Uso: Click Izq)')
+        elif event.button() == Qt.MiddleButton:
+            self.etiqueta.setText('Haga click click! (Uso: Click Middle)')
+        elif event.button() == Qt.RightButton:
+            self.etiqueta.setText('Haga click click! (Uso: Click Right)')
+        
         
     def mouseDoubleClickEvent(self, event):
-        self.etiqueta.setText('Mucho entusiasmo')
+        if event.button() == Qt.LeftButton:
+            self.etiqueta.setText('Mucho entusiasmo (Click Izq)')
+        elif event.button() == Qt.MiddleButton:
+            self.etiqueta.setText('Mucho entusiasmo (Click Middle)')
+        elif event.button() == Qt.RightButton:
+            self.etiqueta.setText('Mucho entusiasmo (Click Right)')
+        
 
 if __name__ == '__main__':
     app = QApplication([])
